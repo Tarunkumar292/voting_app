ac2deb48d9312e25c9d3ce76c7923efc0f9940ff
Tarunkumar292 <tarunkumar2920@gmail.com>
2024-09-26 16:56:01 +0530

    complete voting app

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const bodyparser = require('body-parser')
 app.use(bodyparser.json())
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('public'));
+
 //jwt
 // const { jwtAuthMiddleware } = require('./jwt');
 
